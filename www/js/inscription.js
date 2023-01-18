d8b2e8ab7e8d08c073db333d60f73b518a31f2bc
debrandt <william.debrandt@etudiant.univ-rennes1>
2023-01-18 12:10:54 +0100

    Verification de mot de passe à l'inscription

--- www/js/inscription.js
+++ www/js/inscription.js
@@ -6,25 +6,33 @@ export function renderInscriptionPage($page) {
 function inscriptionSetUp(){
     const $login = $('#login');
     const $password= $('#password');
+    const $passwordVerif = $('#password2');
 
     function submitForm(){
         const login = $login.val();
         const password = $password.val();
+        const passwordVerif = $passwordVerif.val();
 
-        const request = `http://localhost:8080/createUser/${login}/${password}`;
-        fetch(request, {method:'POST'})
-            .then((data) => {
-                return data.json()
-            })
-            .then((json) => {
-                if(json==true){
-                    alert("Bonjour " + login + " , vous avez bien été enregistré.");
-                    window.location.href = "#"
-                }
-                else{
-                    alert("L'utilisateur nommé '"+ login + "' existe déjà");
-                }
-            })
+        if(password !== passwordVerif){
+            alert("Les mots de passe ne correspondent pas");
+        }
+        else{
+            const request = `http://localhost:8080/createUser/${login}/${password}`;
+            fetch(request, {method:'POST'})
+                .then((data) => {
+                    return data.json()
+                })
+                .then((json) => {
+                    if(json==true){
+                        alert("Bonjour " + login + " , vous avez bien été enregistré.");
+                        window.location.href = "#"
+                    }
+                    else{
+                        alert("L'utilisateur nommé '"+ login + "' existe déjà");
+                    }
+                })
+        }
+
     }
 
     $('#Submit').on('touchstart click', function (){
